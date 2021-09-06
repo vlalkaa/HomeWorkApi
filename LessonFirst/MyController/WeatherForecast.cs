09d3e340b08fb6e1717d0ef042cf444ca842560d
Alla Manakova <Allamanakova10@Yandex.ru>
2021-09-06 18:53:56 +0300

    add 1 lesson

---
+++ LessonFirst/MyController/WeatherForecast.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyController
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        private int TemperatureC { get; set; }
+       
+        public void CopyData(WeatherForecast weatherForecast)
+        {
+            Date = weatherForecast.Date;
+            TemperatureC = weatherForecast.TemperatureC;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is WeatherForecast weatherForecast && this == weatherForecast;
+        }
+
+        public override int GetHashCode()
+        {
+            return ~Date.GetHashCode() * TemperatureC.GetHashCode();
+        }
+
+        public static bool operator ==(WeatherForecast w1, WeatherForecast w2)
+        {
+            if (ReferenceEquals(w1, null) || ReferenceEquals(w2, null))
+                return false;
+
+            return w1.Date == w2.Date && w1.TemperatureC == w2.TemperatureC;
+        }
+
+        public static bool operator !=(WeatherForecast w1, WeatherForecast w2)
+        {
+            return !(w1 == w2);
+        }
+    }
+}
